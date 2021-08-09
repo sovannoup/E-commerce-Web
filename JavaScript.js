b94a03ca3accebcfe79b1b43c0c4619f873b2b3a
SOVAN NOUP <sovannoup>
2021-08-09 15:31:08 +0700

    Picture cover slider

--- JavaScript.js
+++ JavaScript.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
     $(".search-mobi").on('click', function(event) {
         $('.phone-search').fadeToggle(200);
     });
+
+    $(".first").on('click', function(event) {
+        $('.first').css("color","#e9a914")
+        $('.second').css("color","black")
+        $('.first-img').css("display","flex")
+        $('.second-img').css('display', 'none');
+    })
+    $(".second").on('click', function(event) {
+        $('.second').css("color","#e9a914")
+        $('.first').css("color","black")
+        $('.first-img').css("display","none")
+        $('.second-img').css('display', 'flex');
+    })
 });
